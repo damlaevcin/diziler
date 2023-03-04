8a355e184dbda1d5aa3e2c0610a752d343562944
damlaevcin <dmlaevcinn@gmail.com>
2023-03-04 22:02:08 +0300

    .

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var students = [12, 15, 18, 11, 10, 9];
+
+console.log(students[6])
+
+if (students[6] >= 18) {
+    alert("sen reşitsin");
+}
+else if (students[6] >= 15 && students[6] < 18 ) {
+    alert("ergensin");
+}
+else {
+    alert("sen insan mısın???");
+}
